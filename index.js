545cb23959907e7522423df3ea209c68f580821a
Galking10 <krllglkn1807@gmail.com>
2022-07-28 16:32:30 +0200

    ready

---
+++ index.js
@@ -0,0 +1,38 @@
+// console.log("Do")
+
+// setTimeout(()=>{
+//     console.log("Work")
+// }, 3000)
+
+// console.log("After")
+
+// const logger = time =>{
+//     console.log(`Лог каждые ${time}`)
+// }
+
+// const timeId = setTimeout(logger, 3000, 2000)
+
+// const shouldCancelTimer = Math.random() > 0.3
+
+
+// console.log(shouldCancelTimer)
+// if(shouldCancelTimer){
+//     clearTimeout(timeId)
+// }
+
+// setInterval(logger, 2000, 2000)
+
+// let counter =0;
+// let hasSubscribe = false
+// const InterId = setInterval(()=>{
+// if(counter === 5 || hasSubscribe){
+//     clearTimeout(InterId)
+// console.log('Stop')
+
+// return
+// }
+// console.log('Work')
+// hasSubscribe = true
+// counter +=1
+// },1000)
+
